a673e0d9172516159653e0ebf94952598f38bfbe
invincible1900 <395984722@qq.com>
2021-06-04 14:12:42 +0800

    update

---
+++ misc/log/main.c
@@ -0,0 +1,44 @@
+/****************************************************************
+ * Desc: 打印有颜色的日志
+ *
+ * Author: X++D
+ *
+****************************************************************/
+
+#include <stdio.h>
+
+#define NONE "\033[m"
+#define RED "\033[0;32;31m"
+#define LIGHT_RED "\033[1;31m"
+#define GREEN "\033[0;32;32m"
+#define LIGHT_GREEN "\033[1;32m"
+#define BLUE "\033[0;32;34m"
+#define LIGHT_BLUE "\033[1;34m"
+#define DARK_GRAY "\033[1;30m"
+#define CYAN "\033[0;36m"
+#define LIGHT_CYAN "\033[1;36m"
+#define PURPLE "\033[0;35m"
+#define LIGHT_PURPLE "\033[1;35m"
+#define BROWN "\033[0;33m"
+#define YELLOW "\033[1;33m"
+#define LIGHT_GRAY "\033[0;37m"
+#define WHITE "\033[1;37m"
+
+#define x_err(fmt, ...) \
+        while(1) { \
+                printf("[D] %s:%d, ", __func__, __LINE__); \
+                printf(RED fmt NONE, ##__VA_ARGS__); \
+                break; \
+        }
+
+#define x_log(fmt, ...) \
+        while(1) { \
+                printf("[D] %s:%d, ", __func__, __LINE__); \
+                printf(YELLOW fmt NONE, ##__VA_ARGS__); \
+                break; \
+        }
+
+int main(){
+	x_log("Hello\n");
+	x_err("Oops\n");
+}
